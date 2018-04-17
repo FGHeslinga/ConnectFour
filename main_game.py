4bb604ba94c4e8365296e704b440dcfc9126d912
FGHeslinga <31314864+FGHeslinga@users.noreply.github.com>
2018-04-17 21:25:11 +0200

    Created main game file

---
+++ main_game.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 17 21:20:13 2018
+
+@author: Friso & Laura
+"""
+
+# Create playing field
+
+
+
+
+
+
+
+
+# Make GUI out of playing field
+
+
+
+
+
+
+
+
+# Let player 1 and player 2 play until 1 player has won
+# So, also check if a player has won
+
+
+
+
+
+
+# Input player 1
+
+
+
+
+# Input player 2
+
+
+
+
+
+
+
+
+
+
