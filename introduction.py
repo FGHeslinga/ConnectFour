3f4031e77b05066da424e7f98ff6548fcba55de5
FGHeslinga <31314864+FGHeslinga@users.noreply.github.com>
2018-04-17 21:15:45 +0200

    removed strange lines

--- introduction.py
+++ introduction.py
@@ -9,8 +9,6 @@ Created on Tue Apr 17 20:43:31 2018
 
 print("Friso is de liefste")
 print("Laura is Lam")
-<<<<<<< HEAD
 print("Allemaal text op regel 12")
-=======
 print("Friso is bijzonder")
->>>>>>> 1e02e65ca33ecb48d8831194256411decf7d813d
+
